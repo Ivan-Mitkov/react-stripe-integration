952e17c3b1d595b852832a07fca839cc4e74c37e
Ivan Ivanov <ivan.ivanov@app-streams.com>
2023-08-02 11:37:59 +0300

    add comment

--- client/src/Payment.js
+++ client/src/Payment.js
@@ -46,8 +46,15 @@ function Payment(props) {
   return (
     <>
       <h1>React Stripe and the Payment Element</h1>
-   {hasStripeAndSecret&&   <Elements stripe={stripePromise} options={{clientSecret}}>
-      <CheckoutForm/>
+      {/**
+       * 
+       * The Elements provider allows you to use Element components and access the Stripe object in any nested component. Render an Elements provider at the root of your React app so that it is available everywhere you need it.
+
+        To use the Elements provider, call loadStripe from @stripe/stripe-js with your publishable key. The loadStripe function asynchronously loads the Stripe.js script and initializes a Stripe object. Pass the returned Promise to Elements.
+       */}
+   {hasStripeAndSecret&&
+      <Elements stripe={stripePromise} options={{clientSecret}}>
+          <CheckoutForm/>
       </Elements>}
     </>
   );
